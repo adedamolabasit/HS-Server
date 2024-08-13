1eaa6c028d3021941317bb94615d376966539c08
adedamolabasit <adedamolabasit09@gmail.com>
2024-08-13 01:47:59 +0100

    ai classifications

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import { classifyHs } from './classifyHs'; // Import your classification function
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
